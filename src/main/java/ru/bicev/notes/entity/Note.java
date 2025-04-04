88f28f6e6da35b1b93f59a7f85caa64368ae7f5b
BicEv <bickbovev@gmail.com>
2025-04-04 15:32:24 +0300

    Added entities

---
+++ src/main/java/ru/bicev/notes/entity/Note.java
@@ -0,0 +1,60 @@
+package ru.bicev.notes.entity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "notes")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "users_id")
+    private User user;
+
+    private String text;
+
+    @ElementCollection
+    @CollectionTable(name = "notes_tags", joinColumns = @JoinColumn(name = "note_id"))
+    @Column(name = "tag")
+    private List<String> tags = new ArrayList<>();
+
+    public Note(User user, String text) {
+        this.user = user;
+        this.text = text;
+    }
+
+    public void addTag(String tag) {
+        this.tags.add(tag);
+    }
+
+    public void addTags(List<String> tags) {
+        this.tags.addAll(tags);
+    }
+
+    public void addTags(String... tags) {
+        Collections.addAll(this.tags, tags);
+    }
+
+}
