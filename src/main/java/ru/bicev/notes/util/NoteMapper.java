32c294b856e2e1836f0cef21b2326df3a63d36af
BicEv <bickbovev@gmail.com>
2025-04-04 20:16:24 +0300

    added method working with Lists

--- src/main/java/ru/bicev/notes/util/NoteMapper.java
+++ src/main/java/ru/bicev/notes/util/NoteMapper.java
@@ -1,5 +1,8 @@
 package ru.bicev.notes.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ru.bicev.notes.dto.NoteDto;
 import ru.bicev.notes.entity.Note;
 import ru.bicev.notes.entity.User;
@@ -27,6 +30,22 @@ public class NoteMapper {
         noteDto.setText(note.getText());
         noteDto.setTags(note.getTags());
         return noteDto;
+    }
+
+    public static List<Note> toEntityList(List<NoteDto> noteDtos, User user) {
+        List<Note> notes = new ArrayList<>();
+        for (NoteDto noteDto : noteDtos) {
+            notes.add(toEntity(noteDto, user));
+        }
+        return notes;
+    }
+
+    public static List<NoteDto> toDtoList(List<Note> notes) {
+        List<NoteDto> noteDtos = new ArrayList<>();
+        for (Note note : notes) {
+            noteDtos.add(toDto(note));
+        }
+        return noteDtos;
     }
 
 }
