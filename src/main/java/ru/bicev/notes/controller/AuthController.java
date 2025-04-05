ccb3a5451b098c469e4a67d465752d076304c1f0
BicEv <bickbovev@gmail.com>
2025-04-05 18:05:18 +0300

    Added Authentication controller

---
+++ src/main/java/ru/bicev/notes/controller/AuthController.java
@@ -0,0 +1,49 @@
+package ru.bicev.notes.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import ru.bicev.notes.dto.UserDto;
+import ru.bicev.notes.service.JwtService;
+import ru.bicev.notes.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class AuthController {
+
+    private final UserService userService;
+    private final AuthenticationManager authManager;
+    private final JwtService jwtService;
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+
+    public AuthController(UserService userService, AuthenticationManager authManager, JwtService jwtService) {
+        this.userService = userService;
+        this.authManager = authManager;
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserDto> registerUser(@RequestParam String email, @RequestParam String password) {
+        UserDto userDto = userService.registerUser(email, password);
+        return new ResponseEntity<>(userDto, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestParam String email, @RequestParam String password) {
+        authManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+
+        String token = jwtService.generateToket(email);
+        return ResponseEntity.ok(token);
+    }
+
+}
