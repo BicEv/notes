572e4ae336f7eac42b0f9cd25247a90b4b447ca0
BicEv <bickbovev@gmail.com>
2025-04-07 18:33:40 +0300

    Added custom setTag to set tags in upper case

--- src/main/java/ru/bicev/notes/dto/NoteDto.java
+++ src/main/java/ru/bicev/notes/dto/NoteDto.java
@@ -1,6 +1,7 @@
 package ru.bicev.notes.dto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -22,5 +23,9 @@ public class NoteDto {
     private String text;
 
     private List<String> tags;
+
+    public void setTags(List<String> tags) {
+        this.tags = tags.stream().map(String::toUpperCase).collect(Collectors.toList());
+    }
 
 }
