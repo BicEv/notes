4f55143fe6130de1de6db1520f8dc048833473dd
BicEv <bickbovev@gmail.com>
2025-04-09 20:50:56 +0300

    Changed logic to prevent filtering in register/login methods

--- src/main/java/ru/bicev/notes/security/JwtFilter.java
+++ src/main/java/ru/bicev/notes/security/JwtFilter.java
@@ -32,6 +32,13 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+
+        String path = request.getServletPath();
+        if (path.equals("/api/users/login") || path.equals("/api/users/register")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String authHeader = request.getHeader("Authorization");
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             String jwt = authHeader.substring(7);
