a897a95431f24dfd08e85b7c124e7f0353a231be
BicEv <bickbovev@gmail.com>
2025-04-11 19:40:20 +0300

    Changed annotations

--- src/main/java/ru/bicev/notes/service/JwtService.java
+++ src/main/java/ru/bicev/notes/service/JwtService.java
@@ -8,11 +8,11 @@ import javax.crypto.SecretKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Jwts;
 
-@Component
+@Service
 public class JwtService {
 
     private final SecretKey secretKey;
