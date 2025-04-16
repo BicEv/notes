22dd6824cf1af975fefb607399aa756937ca101a
BicEv <bickbovev@gmail.com>
2025-04-16 18:30:12 +0300

    Added proper errorResponse dto

---
+++ src/main/java/ru/bicev/notes/dto/ErrorResponse.java
@@ -0,0 +1,22 @@
+package ru.bicev.notes.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private String message;
+
+    private int statusCode;
+
+    private LocalDateTime timestamp;
+
+}
